9d6d6bb65d5a375c933084a14092e231311ca781
Sulagna Pradhan <infinityresources81@gmail.com>
2025-11-29 22:22:25 +0530

    firebase config move to env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBmFdMmMKLOc_DNbT2cgMwc9_GhGIjwVsk",
-dbAuthDomain: "cusaathi-frontend.firebaseapp.com",
-projectId: "cusaathi-frontend",
-storageBucket: "cusaathi-frontend.firebasestorage.app",
-messagingSenderId: "780693552594",
-appId: "1:780693552594:web:fb86c6ab4520cd9de7dc0b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
